34391898d4d27f138c748966c04be0efce8ac4fd
Steven Speicher <stevenspeicher@gmail.com>
2015-07-09 10:30:36 -0700

    more templating and views for camera archives

--- potd/timelapse/urls.py
+++ potd/timelapse/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^archives/$', views.archive, name="archive"),
+    url(r'^archives/(?P<slug>[-\w]+)/$', views.archive_camera, name="archive_camera"),
+    url(r'^archives/(?P<slug>[-\w]+)/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.archive_camera_day, name="archive_camera_day"),
     url(r'^(?P<slug>[-\w]+)/$', views.camera, name='camera'),
-    url(r'^archive/(?P<slug>[-\w]+)/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.archive_camera_day, name="archive_camera_day"),
 ]
